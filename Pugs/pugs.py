94ff57f153e26d0101452495441cf75f93072b66
Naufal <naufalh39@gmail.com>
2021-02-27 22:54:56 +0700

    requirements update

--- Pugs/pugs.py
+++ Pugs/pugs.py
@@ -6,7 +6,7 @@ from redbot.core.data_manager import cog_data_path
 from google.oauth2.service_account import Credentials
 
 class Pugs(commands.Cog):
-    """My custom cog test"""
+    """My custom cog test 2"""
 
     def __init__(self, bot):
         self.bot = bot
