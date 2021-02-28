3ce04ac28b335cfed7b664e97fed5d5e81fe7c42
Naufal <naufalh39@gmail.com>
2021-02-28 18:16:58 +0700

    async fix

--- Trakteer/trakteer.py
+++ Trakteer/trakteer.py
@@ -8,28 +8,23 @@ class Trakteer(commands.Cog):
     # init method or constructor
     def __init__(self, bot):
         self.bot = bot
-        print("Initialized")
         self.bot.loop.create_task(self.wsrun('wss://socket.trakteer.id/app/2ae25d102cc6cd41100a'))
 
     async def wsrun(self, uri):
-        print("Running wsrun")
         async with websockets.connect(uri) as self.websocket:
             await self.websocket.send('{"event":"pusher:subscribe","data":{"channel":"creator-stream.n8rx3ldzx7o4wamg.trstream-t6ZPmsNYQM061wcg5slw"}}')
             await self.websocket.send('{"event":"pusher:subscribe","data":{"channel":"creator-stream-test.n8rx3ldzx7o4wamg.trstream-t6ZPmsNYQM061wcg5slw"}}')
-            print("Sockets sent")
             while True:
-                print("Looping")
                 resp = json.loads(await self.websocket.recv())
-                print(resp)
                 if resp['event'] == "Illuminate\\Notifications\\Events\\BroadcastNotificationCreated":
-                    print(resp['data'])
+                    #print(resp['data'])
                     donator = json.loads(resp['data'])
 
-                    embed = discord.Embed(color=0xEE2222, title='%s mentraktir %s %s' % (donator['supporter_name'], donator['quantity'], donator['supporter_message']), timestamp=datetime.datetime.utcnow())
+                    embed = discord.Embed(color=0xEE2222, title='%s mentraktir %s %s' % (donator['supporter_name'], donator['quantity'], donator['unit']), timestamp=datetime.datetime.utcnow())
                     embed.description = donator['supporter_message']
                     embed.set_thumbnail(url=donator['unit_icon'])
                     embed.set_author(name='Pick-Up Games Registration', icon_url='https://i.imgur.com/kgrkybF.png')
-                    embed.set_footer(text=donator['price'], icon_url='https://www.flaticon.com/svg/vstatic/svg/715/715709.svg?token=exp=1614506121~hmac=3f881af053c9eb64afbd8f666b5b60fb')
+                    embed.set_footer(text=donator['price'], icon_url='https://i.imgur.com/0b4L7uL.png')
                     await self.bot.get_channel(653090156961857539).send(embed=embed)
 
     def cog_unload(self):
