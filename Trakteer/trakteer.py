302289a0e9098b1cd19c29d7073f06247303b4ca
Naufal <naufalh39@gmail.com>
2021-03-02 10:12:54 +0700

    trakteer update

--- Trakteer/trakteer.py
+++ Trakteer/trakteer.py
@@ -2,6 +2,7 @@ import discord
 import datetime
 import websockets
 import json
+import asyncio
 from redbot.core import commands
 
 class Trakteer(commands.Cog):
@@ -15,19 +16,32 @@ class Trakteer(commands.Cog):
             await self.websocket.send('{"event":"pusher:subscribe","data":{"channel":"creator-stream.n8rx3ldzx7o4wamg.trstream-t6ZPmsNYQM061wcg5slw"}}')
             await self.websocket.send('{"event":"pusher:subscribe","data":{"channel":"creator-stream-test.n8rx3ldzx7o4wamg.trstream-t6ZPmsNYQM061wcg5slw"}}')
             while True:
-                resp = json.loads(await self.websocket.recv())
-                if resp['event'] == "Illuminate\\Notifications\\Events\\BroadcastNotificationCreated":
-                    donator = json.loads(resp['data'])
-                    embed = discord.Embed(color=0xEE2222, title='%s mentraktir %s %s' % (donator['supporter_name'], donator['quantity'], donator['unit']), timestamp=datetime.datetime.utcnow())
-                    embed.url = 'https://trakteer.id/overwatch-idn/'
-                    embed.description = 'Baru saja memberikan **%s**' % donator['price']
-                    embed.set_thumbnail(url=donator['unit_icon'])
-                    embed.add_field(name='Klik disini untuk ikut mentraktir',value='https://trakteer.id/overwatch-idn/')
+                try:
+                    resp = json.loads(await self.websocket.recv())
+                    if resp['event'] == "Illuminate\\Notifications\\Events\\BroadcastNotificationCreated":
+                        donator = json.loads(resp['data'])
+                        '''
+                        embed = discord.Embed(color=0xEE2222, title='%s mentraktir %s %s' % (donator['supporter_name'], donator['quantity'], donator['unit']), timestamp=datetime.datetime.utcnow())
+                        embed.url = 'https://trakteer.id/overwatch-idn/'
+                        embed.description = 'Baru saja memberikan **%s**' % donator['price']
+                        embed.set_thumbnail(url=donator['unit_icon'])
+                        embed.add_field(name='Klik disini untuk ikut mentraktir',value='https://trakteer.id/overwatch-idn/')
+                        '''
 
-                    if 'supporter_message' in donator and len(donator['supporter_message']) > 0:
-                        embed.set_footer(text= donator['supporter_message'], icon_url=donator['supporter_avatar'])
+                        embed = discord.Embed(color=0xEE2222, title='%s baru saja mengirimkan %s %s' % (donator['supporter_name'], donator['price']), timestamp=datetime.datetime.utcnow())
+                        embed.description = '👉 https://trakteer.id/overwatch-idn/'
+                        embed.set_thumbnail(url=donator['unit_icon'])
+                        embed.set_author(name='Donation Box', icon_url='https://i.imgur.com/kgrkybF.png')
 
-                    await self.bot.get_channel(803626623596363786).send(embed=embed)
+                        if 'supporter_message' in donator and len(donator['supporter_message']) > 0:
+                            embed.set_footer(text= donator['supporter_message'], icon_url=donator['supporter_avatar'])
+
+                        await self.bot.get_channel(803626623596363786).send(embed=embed)
+                except websockets.exceptions.ConnectionClosed:
+                    await asyncio.sleep(5)
+                    await self.wsrun(uri)
+                    print(">>>>>>>>>>>>>>>>>>>>>Reload")
+                    break
 
     def cog_unload(self):
         self.socket_task.cancel()
