db7a39097715adb785b23d5ac444d28d174442a6
Naufal <naufalh39@gmail.com>
2021-05-07 11:03:00 +0700

    IPN for reila

--- IPN/IPN.py
+++ IPN/IPN.py
@@ -42,10 +42,10 @@ class IPN(commands.Cog):
 
     async def wsrun(self):
         try:
-            async with websockets.serve(self.listen, "localhost", 8887):
+            await websockets.serve(self.listen, "localhost", 8887)
                 #self.stop_event = threading.Event()
                 #self.bot.loop.run_in_executor(None, self.stop_event.wait)
-                self.log.debug("Stop stop stop")
+                #self.log.debug("Stop stop stop")
             self.log.debug("[IPN] PayPal IPN websocket server started on port 8887")
             while True:
                 await asyncio.sleep(1)
