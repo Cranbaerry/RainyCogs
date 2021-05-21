6de9337f46c82319476a7326e7c037396e418950
Naufal <naufalh39@gmail.com>
2021-05-21 23:53:08 +0700

    tiktok thing

--- tiktok/tiktok.py
+++ tiktok/tiktok.py
@@ -34,8 +34,8 @@ class TikTok(commands.Cog):
             self.config.register_guild(subscriptions=[], cache=[])
             self.config.register_global(interval=300, cache_size=500, proxy=[])
             #self.background_get_new_videos.start()
-            self.bot.loop.create_task(self.initialize())
-            self.bot.loop.create_task(self.background_get_new_videos())
+            self.main_task = self.bot.loop.create_task(self.initialize())
+            self.background_task = self.bot.loop.create_task(self.background_get_new_videos())
 
     async def initialize(self):
         await self.bot.wait_until_red_ready()
@@ -109,63 +109,11 @@ class TikTok(commands.Cog):
             self.log.debug(f"Sleeping {interval} seconds..")
             await asyncio.sleep(interval)
 
-    @tasks.loop(seconds=300)
-    async def background_get_new_videos2(self):
-        self.log.debug("Running background..")
-        for guild in self.bot.guilds:
-            try:
-                subs = await self.config.guild(guild).subscriptions()
-                cache = await self.config.guild(guild).cache()
-            except:
-                self.log.debug("Unable to fetch data, config is empty..")
-                return
-            for i, sub in enumerate(subs):
-                self.log.debug(f"Fetching data of {sub['id']} from guild channel: {sub['channel']['name']}")
-                channel = self.bot.get_channel(int(sub["channel"]["id"]))
-                tiktoks = await self.get_tiktok_by_name(sub["id"], 3)
-                '''#try:
-                   
-                except TikTokCaptchaError:
-                    self.log.error("Asking captcha, need proxy")
-                    continue
-                self.log.debug("Response: " + str(tiktoks))
-                if not channel:
-                    self.log.debug("Channel not found: " + sub["channel"]["name"])
-                    continue
-                self.log.debug("Items: " + str(cache))
-                for post in tiktoks:
-                    self.log.debug("Post ID: " + post["id"])
-                    self.log.debug("Post Content: " + str(post))
-                    if not post["id"] in cache:
-                        self.log.debug("Sending data to channel: " + sub["channel"]["name"])
-                        # TODO: Send embed and post in channel
-                        # Add id to published cache
-                        cache.append(post["id"])
-                        await self.config.guild(guild).cache.set(cache)
-                        self.log.debug("Saved cache data: " + str(cache))
-                    else:
-                        self.log.debug("Skipping: " + post["id"])'''
-
-                self.log.debug("Sleeping 5 seconds..")
-                await asyncio.sleep(5)
-
-
-    #@background_get_new_videos.before_loop
-    async def wait_for_red(self):
-        await self.bot.wait_until_red_ready()
-        self.log.debug(f"Ready")
-        self.log.debug(f"Ready")
-        self.log.debug(f"Ready")
-        self.log.debug(f"Ready")
-        self.log.debug(f"Ready")
-        self.log.debug(f"Ready")
-        interval = await self.config.interval()
-        self.log.debug(f"Background process interval is set to {interval}")
-        self.background_get_new_videos.change_interval(seconds=interval)
-
     def cog_unload(self):
         self.log.debug("Shutting down TikTok service..")
-        self.background_get_new_videos.cancel()
+        self.background_task.cancel()
+        self.main_task.cancel()
+        #self.background_get_new_videos.cancel()
         # self.api.browser.browser.quit()
 
     @commands.group()
