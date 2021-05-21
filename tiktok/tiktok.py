7fe268d5331dbb7d3638eb5856813a0d44cd079a
Naufal <naufalh39@gmail.com>
2021-05-21 20:28:21 +0700

    tiktok thing

--- tiktok/tiktok.py
+++ tiktok/tiktok.py
@@ -30,10 +30,8 @@ class TikTok(commands.Cog):
             self.config = Config.get_conf(self, identifier=UNIQUE_ID, force_registration=True)
             self.config.register_guild(subscriptions=[], cache=[])
             self.config.register_global(interval=300, cache_size=500, proxy=[])
+            self.bot.loop.create_task(self.initialize())
             self.background_get_new_videos.start()
-            self.initialize()
-            #self.init_task = self.bot.loop.create_task()
-            #self.init_task.start()
 
     async def initialize(self):
         await self.bot.wait_until_red_ready()
