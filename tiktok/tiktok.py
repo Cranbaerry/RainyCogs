972e291fbc8c11d24f5e36d6328125b7da764f1c
Naufal <naufalh39@gmail.com>
2021-05-23 12:54:39 +0700

    tiktok thing

--- tiktok/tiktok.py
+++ tiktok/tiktok.py
@@ -124,7 +124,7 @@ class TikTok(commands.Cog):
             else:
                 break
 
-        #driver.quit()
+        driver.quit()
         return cookie
 
     async def get_new_proxy(self, proxies, truncate=False):
@@ -189,7 +189,7 @@ class TikTok(commands.Cog):
                 self.log.warning("Unable to fetch data, config is empty..")
                 return
             for i, sub in enumerate(subs):
-                # self.log.debug(f"Retrieving data of {sub['id']} from guild channel: {sub['channel']['name']}")
+                self.log.debug(f"Retrieving data of {sub['id']} from guild channel: {sub['channel']['name']}")
                 channel = self.bot.get_channel(int(sub["channel"]["id"]))
                 while True:
                     try:
@@ -222,7 +222,9 @@ class TikTok(commands.Cog):
                     self.log.warning("TikTok channel not found: " + sub["id"])
                     continue
 
+                self.log.debug("A")
                 self.log.debug(f"Retrieved {len([x for x in tiktoks if not x in cache])} video posts from {sub['id']} for {sub['channel']['name']} ({sub['channel']['id']})")
+                self.log.debug("B")
                 for post in tiktoks:
                     if not post["id"] in cache:
                         self.log.debug(f"Posting {post['id']} to the channel")
